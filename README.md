7c70813e5eb271daa0b0b0204208eed8ac6d5915
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-06-30 10:24:15 +0100

    translated linesearch

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Eventually this code will be integrated into [NLsolve.jl](https://github.com/Jul
 
 
 
-[![Build Status](https://travis-ci.org/cortner/CTKSolvers.jl.svg?branch=master)](https://travis-ci.org/cortner/CTKSolvers.jl)
+<!-- [![Build Status](https://travis-ci.org/cortner/CTKSolvers.jl.svg?branch=master)](https://travis-ci.org/cortner/CTKSolvers.jl)
 
 [![Coverage Status](https://coveralls.io/repos/cortner/CTKSolvers.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/cortner/CTKSolvers.jl?branch=master)
 
-[![codecov.io](http://codecov.io/github/cortner/CTKSolvers.jl/coverage.svg?branch=master)](http://codecov.io/github/cortner/CTKSolvers.jl?branch=master)
+[![codecov.io](http://codecov.io/github/cortner/CTKSolvers.jl/coverage.svg?branch=master)](http://codecov.io/github/cortner/CTKSolvers.jl?branch=master) -->
--- src/linesearch.jl
+++ src/linesearch.jl
@@ -1,45 +1,43 @@
 
-function lambdap = parab3p(lambdac, lambdam, ff0, ffc, ffm)
-% Apply three-point safeguarded parabolic model for a line search.
-%
-% C. T. Kelley, April 1, 2003
-%
-% This code comes with no guarantee or warranty of any kind.
-%
-% function lambdap = parab3p(lambdac, lambdam, ff0, ffc, ffm)
-%
-% input:
-%       lambdac = current steplength
-%       lambdam = previous steplength
-%       ff0 = value of \| F(x_c) \|^2
-%       ffc = value of \| F(x_c + \lambdac d) \|^2
-%       ffm = value of \| F(x_c + \lambdam d) \|^2
-%
-% output:
-%       lambdap = new value of lambda given parabolic model
-%
-% internal parameters:
-%       sigma0 = .1, sigma1 = .5, safeguarding bounds for the linesearch
-%
+"""
+`function  parab3p(lambdac, lambdam, ff0, ffc, ffm) -> lambdap`
 
-%
-% set internal parameters
-%
-sigma0 = .1; sigma1 = .5;
-%
-% compute coefficients of interpolation polynomial
-%
-% p(lambda) = ff0 + (c1 lambda + c2 lambda^2)/d1
-%
-% d1 = (lambdac - lambdam)*lambdac*lambdam < 0
-%      so if c2 > 0 we have negative curvature and default to
-%      lambdap = sigam1 * lambda
-%
-c2 = lambdam*(ffc-ff0)-lambdac*(ffm-ff0);
-if c2 >= 0
-    lambdap = sigma1*lambdac; return
-end
-c1 = lambdac*lambdac*(ffm-ff0)-lambdam*lambdam*(ffc-ff0);
-lambdap = -c1*.5/c2;
-if lambdap < sigma0*lambdac, lambdap = sigma0*lambdac; end
-if lambdap > sigma1*lambdac, lambdap = sigma1*lambdac; end
+Apply three-point safeguarded parabolic model for a line search.
+
+### Input Parameters
+
+* `lambdac` : current steplength
+* `lambdam` : previous steplength
+* `ff0` : value of |F(x_c)|^2
+* `ffc` : value of |F(x_c + λc d)|^2
+* `ffm` : value of |F(x_c + λm d)|^2
+
+### Output
+
+* lambdap : new value of lambda given parabolic model
+
+### Keyword arguments (internal parameters)
+
+* `sigma0 = .1`, `sigma1 = .5` : safeguarding bounds for the linesearch
+"""
+function parab3p(lambdac, lambdam, ff0, ffc, ffm;
+                        sigma0 = 0.1, sigma1 = 0.5) # -> lambdap
+   # compute coefficients of interpolation polynomial
+   # p(lambda) = ff0 + (c1 lambda + c2 lambda^2)/d1
+   # d1 = (lambdac - lambdam)*lambdac*lambdam < 0
+   #      so if c2 > 0 we have negative curvature and default to
+   #      lambdap = sigam1 * lambda
+   c2 = lambdam * (ffc - ff0) - lambdac * (ffm - ff0)
+   if c2 >= 0
+      return sigma1 * lambdac
+   end
+   c1 = lambdac^2 * (ffm-ff0) - lambdam^2 * (ffc - ff0)
+   lambdap = -c1 * 0.5 / c2
+   if lambdap < sigma0 * lambdac
+      return sigma0 * lambdac
+   elseif lambdap > sigma1 * lambdac
+      return sigma1 * lambdac
+   else
+      return lambdap
+   end
+end 
