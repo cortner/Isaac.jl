8bbbc46be2929ebc2597e851ef4ad68807d49239
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-08-05 21:58:07 +0100

    begin to port and generalise the dlanczos algorithm

--- src/fdkrylov.jl
+++ src/fdkrylov.jl
@@ -1,5 +1,6 @@
 
-export dgmres
+
+export dgmres, dlanczos
 
 
 """
@@ -18,7 +19,7 @@ Finite difference directional derivative, approximate f'(x) w
 
 based on a code by C. T. Kelley, April 1, 2003
 """
-function dirder(x, w, f, f0; h = 1e-7)
+function dirder(x, w, f, f0, h)
    # initialise difference increment
    epsnew = h
    n = length(x)
@@ -34,6 +35,17 @@ function dirder(x, w, f, f0; h = 1e-7)
    epsnew /= norm(w)
    f1 = f(x + epsnew * w)
    return (f1 - f0) / epsnew
+end
+
+
+"""
+`dirderinf(f, f0, xc, v, h)`:
+directional derivative operator to mimic the ∂f(xc) * v action
+via (f(x+h'*v) - f(x)) / h' where h' is rescaled against ||v||_∞
+"""
+function dirderinf(f, f0, xc, v, h)
+   h = h / norm(v, Inf)
+   return (f(xc + h * v) - f0) / h
 end
 
 
@@ -86,11 +98,11 @@ GMRES linear equation solver for use in Newton-GMRES solver
 * error : vector of residual norms for the history of the iteration
 * total_iters : number of iterations
 """
-function dgmres(f0, f, xc, errtol, kmax, reorth = 1, x = zeros(f0))
+function dgmres(f0, f, xc, errtol, kmax, reorth = 1, x = zeros(f0); h = 1e-7)
    # The right side of the linear equation for the step is -f0.
    b = - f0
    n = length(b)
-   r = - dirder(xc, x, f, f0) - f0
+   r = - dirder(xc, x, f, f0, h) - f0
 
    h = zeros(kmax, kmax)
    v = zeros(n, kmax)
@@ -118,7 +130,7 @@ function dgmres(f0, f, xc, errtol, kmax, reorth = 1, x = zeros(f0))
       k = k+1;
 
       # Call directional derivative function.
-      v[:, k+1] = dirder(xc, v[:,k], f, f0)
+      v[:, k+1] = dirder(xc, v[:,k], f, f0, h)
       normav = norm(v[:,k+1])
 
       # Modified Gram-Schmidt
@@ -176,78 +188,396 @@ end
 
 
 
+export NK, blocklanczos
+
+
+"""
+`pushcol(V::Matrix, v::Vector) -> V`:
+append the vector `v` as a column to `V`
+"""
+function pushcol(V::Matrix, v::Vector)
+   rows, cols = size(V)
+   V = V[:]
+   append!(V, v)
+   return reshape(V, rows, cols+1)
+end
+
+
+"""
+`sorted_eig(A::Union{SymTridiagonal, Symmetric})`:
+diagonalise A, and return sorted eigenvalues and analogously sorted eigenvectors
+"""
+function sorted_eig(A::Union{SymTridiagonal, Symmetric})
+   D, Q = eig(A)
+   I = sortperm(D)
+   return D[I], Q[:, I]
+end
+
+nkdualnorm(P, f) = norm(f)
+
+"""
+`orthogonalise(w, V::Matrix, P)`:
+Assuming V is P-orthogonal, P-orthogonalise w to V and return
+"""
+function orthogonalise(w, V::Matrix, P)
+   # make w orthogonal to all columns of V
+   for i = 1:size(V,2)
+      w -= dot(w, P, V[:,i]) * V[:,i]
+      # w /= norm(P, w)
+   end
+   if norm(P, w) < 1e-10
+      warn("||w|| is very small after orthogonalisation")
+   end
+   w /= norm(P,w)
+   # TODO: create a test that the w we get is really orthogonal,
+   #       and if not then re-orthogonalise
+   return w
+end
+
+
+"""
+`appendkrylov(V, AxV, Y, v, Hmul, P)`:
+appends [V v], [AxV A*v], [Y P \ A*v]
+"""
+function appendkrylov(V, AxV, Y, v, Hmul, P)
+   V   = pushcol(V,   v)
+   AxV = pushcol(AxV, Hmul(v))
+   Y   = pushcol(Y,   P \ AxV[:, end])
+   return V, AxV, Y
+end
+
+
+# TODO: generalise blocklanczos to allow arbitrary transformations of the
+#       spectrum, and probably also arbitrary right-hand sides
+# TODO: look into re-orthogonalising
+
+"""
+dlanczos(f0, f, xc, errtol, kmax; kwargs...) ->
+
+Given E : ℝᴺ → ℝ with hessian H = ∇²E(x) and b ∈ ℝᴺ  `dlanczos` computes an
+approximate solution `u` to the system
+   g(H) u = b
+where g(H) ∈ ℝᴺˣᴺ is a transformation of J = Q D Q'  of the form
+   g(H) = Q g(D) Q'
+Here, D = diag(λ₁, λ₂, …) is the diagonal matrix of ordered eigenvalues
+(λ₁ being the smallest). More on the transformation g see below.
+
+`dlanczos` first uses a (possibly preconditioned) (block-)lanczos method to compute
+a reasonable approximation to H in the form H = P V T V' P. Then it diagonalises
+T = Q D Q', replaces D with g(D) as above. This gives an approximation G to g(H).
+The method terminates if `pinv(G) b` yields a solution of sufficiently high accuracy
+(residual).
+
+In the lanzcos iterations the matrix vector product H * u
+is replaced with the finite-difference operation (∇E(xc + h u) - ∇E(xc))/h;
+see also `dirder` and `dirderinf`.
+
+dgmres(f0, f, xc, errtol, kmax, reorth = 1, x = zeros(f0))
+
+## Required Parameters
+
+* f0 : ∇E(xc)
+* f : function to evaluate ∇E
+* xc : current point
+* b : right-hand side in the equation
+* errtol : max-norm residual tolerance for termination
+* kmax : maximum number of lanzcos iterations
+
+## KW parameters
+
+* `P` : preconditioner (default: I;  minimally needs to define `*` and `\` )
+* `V0` : initial subspace (default: P \ b)
+* `eigatol`, `eigrtol`: tolerance on the first eigenvalue
+* `debug`: show debug information (true/false)
+* `h` : finite-difference parameter
+* `dirder` : function to compute the directional derivative; see
+         `dirderinf` for format
+* `ORTHTOL` : orthogonality tolerance parameter
+* `Hmul` : the finite-difference operation can in principle be replaces by
+         and arbitrary operator specifying the matrix-vector product
+
+## Returns
+
+* x : approximate solution
+* G : of type `LanczosMatrix`, to extract information about the computed operator
+* success : true if termination criteria are satisfied, false if kmax is reached
+
+## Further Notes
+
+* Acknowledgement: This code is inspired by C T Kelley's Matlab Newton-Krylov solver `nsoli.m`;
+see also `nsoli` and `dgmres` which are directly ported with permission from `nsoli.m`.
+* Termination criterion: If g ≠ id then G * u - b is not in fact the residual
+of the equation we are trying to solve but only an approximation to that residual.
+Therefore the termination criterion is only approximately satisfied.
+"""
+function dlanczos( f0, f, xc, b, errtol, kmax;
+                  P = I, V0 = P \ b,
+                  eigatol = 1e-1, eigrtol = 1e-1,
+                  debug = false, h = 1e-7,
+                  dirder = dirderinf,
+                  Hmul = z -> dirder(f, f0, xc, z, h),
+                  ORTHTOL = 1e-12 )
+
+   if isa(V0, Vector)
+      V0 = reshape(V0, length(V0), 1)
+   end
+   p = size(V0,2)
+
+   # initialise some variables
+   d = length(f0)      # problem dimension
+   @assert kmax <= d
+   numf = 0            # count f evaluations
+   isnewton = false    # remember whether the output is a newton direction
+
+   # allocate Krylov subspace and more
+   V = zeros(d,0)      # store the Krylov basis
+   AxV = zeros(d, 0)   # store A vⱼ
+   Y = zeros(d, 0)     # store P \ A vⱼ
+
+   # initialise Krylov subspace
+   for j = 1:size(V0, 2)
+      vj = orthogonalise(V0[:,j], V, P)
+      nrmvj = norm(P, vj)
+      if nrmvj > ORTHTOL
+         V, AxV, Y = appendkrylov(V, AxV, Y, vj/nrmvj, Hmul, P)
+         numf += 1
+      else
+         warn("a column of V0 is linearly dependent on the rest so I am skipping it")
+      end
+   end
+
+   # prepare for the Block-Lanczos loop
+   j = 1
+   x = zeros(d)
+   vmin = zeros(d)
+   λ = minimum(eigvals(Symmetric(V'*AxV)))
+   λ_old = Float64[]
+
+   # start the block-lanczos loop; when we have kmax v-vectors we stop
+   while size(V, 2) <= kmax
+
+      # We have V, AxV and Y = P \ AxV  available, so we can immediately solve
+      # the projected linear system and eigenvalue problem
+      #
+      # there is probably an elegant way to assemble the projected
+      # linear system, but for now we just do it brute-force:
+      #    A vj = P V T V' P vj
+      #    yj = P \ A qj = V T  V' P vj
+      #    V' P Y = V' P V T V' P V = T
+      n = size(V, 2)
+      T = Symmetric(V' * AxV)       # T = V' * P * Y = V' * AV
+      #  TODO: could replace above with with [dot(V[:,a], P, Y[:,b] for a=1:n, b=1:n]
+
+      # make the index-1 transformation
+      D, Q = sorted_eig(T)
+      E = [ - abs(D[1]); abs(D[2:end]) ]
+      vmin, vmin_old = V * Q[:,1], vmin    # smallest (hopefully unstable) eigenmode
+      # residual estimate for the old x    >>> TODO: should we switch to P^{-1}-norm?
+      res = norm( P * (V * (Q * (E .* (Q' * (V' * (P * x)))))) - b )
+      # new x and λ (remember the old)
+      g = Q * (E .\ (Q' * (V' * b)))
+      x, x_old = V * g, x
+      push!(λ_old, λ)
+      λ = D[1]
+      # if E == D then A' = A hence we can estimate the *actual* and *current* residual
+      if (isnewton = (E == D))
+         res = norm( AxV * g - b )    # TODO: should we switch to P^{-1}-norm?
+      end
+      # check for termination
+      err_λ = maxabs(λ - λ_old[max(1,length(λ)-p+1):end])
+      if res < errtol && (λ < 0 || err_λ < eigatol + eigrtol * abs(λ))
+         return x, λ, vmin, numf, isnewton
+      end
+
+      if debug
+         @printf("    %d   %.2f   %.2e    %.2e \n", j, λ, err_λ, res/norm(b))
+      end
+      # add the next Krylov vector
+      w = orthogonalise(Y[:, j], V, P)
+      nrmw = norm(P, w)
+      if nrmw > ORTHTOL
+         V, AxV, Y = appendkrylov(V, AxV, Y, w/nrmw, Hmul, P)
+         numf += 1
+      end
+      j += 1
+      if j > size(V,2)   # if we have no new vector left, then we are in trouble!
+         # TODO: deal with this somehow, probably by adding a random vector!
+         error("I don't know yet how to deal with the case p = 0")
+      end
+   end
+   # if we are here it means that kmax is reached, i.e. we terminate with
+   # warning or error >>> TODO: return to how to best handle this?!
+   # warn("`dcg_index1` did not converge within kmax = $(kmax) iterations")
+   return x, λ, vmin, numf, isnewton
+end
 
 
 
-# function [step, errstep, total_iters, f_evals] = ...
-#     dkrylov(f0, f, x, params, lmeth)
-# % Krylov linear equation solver for use in nsoli
-# %
-# % C. T. Kelley, April 1, 2003
-# %
-# %
-# % This code comes with no guarantee or warranty of any kind.
-# %
-# % function [step, errstep, total_iters, f_evals]
-# %                              = dkrylov(f0, f, x, params, lmeth)
-# %
-# %
-# % Input:  f0 = function at current point
-# %         f = nonlinear function
-# %              the format for f is  function fx = f(x)
-# %              Note that for Newton-GMRES we incorporate any
-# %              preconditioning into the function routine.
-# %         x = current point
-# %         params = vector to control iteration
-# %              params(1) = relative residual reduction factor
-# %              params(2) = max number of iterations
-# %              params(3) = max number of restarts for GMRES(m)
-# %              params(4) (Optional) = reorthogonalization method in GMRES
-# %                   1 -- Brown/Hindmarsh condition (default)
-# %                   2 -- Never reorthogonalize (not recommended)
-# %                   3 -- Always reorthogonalize (not cheap!)
-# %
-# %         lmeth = method choice
-# %              1 GMRES without restarts (default)
-# %              2 GMRES(m), m = params(2) and the maximum number
-# %                   of restarts is params(3)
-# %              3 Bi-CGSTAB
-# %              4 TFQMR
-# %
-# % Output: x = solution
-# %         errstep = vector of residual norms for the history of
-# %                 the iteration
-# %         total_iters = number of iterations
-# %
-# %
-#
-# %
-# # % initialization
-# %
-# lmaxit = params(2);
-# restart_limit = 20;
-# if length(params) >= 3
-#     restart_limit = params(3);
-# end
-# if lmeth == 1, restart_limit = 0; end
-# if length(params) == 3
-# %
-# % default reorthogonalization
-# %
-#      gmparms = [params(1), params(2), 1];
-# elseif length(params) == 4
-# %
-# % reorthogonalization method is params(4)
-# %
-#      gmparms = [params(1), params(2), params(4)];
-# else
-#      gmparms = [params(1), params(2)];
-# end
-#
-# %
-# % compute the step using a GMRES routine especially designed
-# % for this purpose
-# %
+"""
+Newton-Krylov based saddle search method
+"""
+@with_kw type NK
+   tol::Float64 = 1e-5
+   maxnumdE::Int = 1000
+   len::Float64 = 1e-7
+   precon = I
+   precon_prep! = (P, x) -> P
+   verbose::Int = 1
+   krylovinit::Symbol = :resrot  # allow res, rand, rot, resrot
+   maxstep::Float64 = Inf
+   eigatol::Float64 = 1e-1
+   eigrtol::Float64 = 1e-1
+end
 
 
-   #  [step, errstep, f_evals] = dgmres(f0, f, x, gmparms);
+function run!{T}(method::NK, E, dE, x0::Vector{T},
+                  v0::Vector{T} = rand(T, length(x0)) )
+   # get parameters
+   @unpack tol, maxnumdE, len, verbose, krylovinit, maxstep,
+      eigatol, eigrtol = method
+   precon = x -> method.precon_prep!(method.precon, x)
+   debug = verbose > 2
+
+   # initialise some more parameters; TODO: move these into NK?
+   d = length(x0)
+   eta = etamax = 0.5   # 0.9
+   gamma = 0.9
+   kmax = min(40, d)
+   Carmijo = 1e-4
+   α_old = α = 1.0
+
+   # evaluate the initial residual
+   x = copy(x0)
+   v = copy(v0)
+   f0 = dE(x)
+   numdE = 1
+   res = norm(f0, Inf)
+
+   P = precon(x)
+   fnrm = nkdualnorm(P, f0)
+   fnrmo = 1.0
+   itc = 0
+
+   while res > tol && numdE < maxnumdE
+      rat = fnrm / fnrmo   # TODO: is this unused?
+      fnrmo = fnrm         # TODO: probably move this to where fnrm is updated!
+      itc += 1
+
+      if debug; @show dot(f0, v); end
+      # compute the (modified) Newton direction
+      if krylovinit == :res
+         V0 = - P \ f0
+      elseif krylovinit == :rand
+         V0 = P \ rand(d)
+      elseif krylovinit == :rot
+         V0 = v
+      elseif krylovinit == :resrot
+         V0 = [ P \ f0 v ]
+      else
+         error("unknown parameter `krylovinit = $(krylovinit)`")
+      end
+      p, λ, v, inner_numdE, isnewton =
+            blocklanczos(f0, dE, x, eta * norm(f0), kmax;
+                         P = P, b = - f0, V0 = V0, debug = (verbose >= 3),
+                         h = len, eigatol = eigatol, eigrtol = eigrtol)
+      numdE += inner_numdE
+      if debug; @show isnewton; end
+
+      # ~~~~~~~~~~~~~~~~~~ LINESEARCH ~~~~~~~~~~~~~~~~~~~~~~
+      if isnewton
+         iarm = 0
+         α = αt = 1.0
+         xt = x + αt * p
+         ft = dE(xt)
+         numdE += 1
+         nf0 = nkdualnorm(P, f0)
+         nft = nkdualnorm(P, ft)
+
+         αm = 1.0  # these have no meaning; just allocations
+         nfm = 0.0
+
+         while nft > (1 - Carmijo * αt) * nf0
+            if iarm == 0
+               α *= 0.5
+            else
+               α = parab3p( αt, αm, nf0^2, nft^2, nfm^2; sigma0 = 0.1, sigma1 = 0.5 )
+            end
+            αt, αm, nfm = α, αt, nft
+            if αt < 1e-8   # TODO: make this a parameter
+               error(" Armijo failure, step-size too small")
+            end
+
+            xt = x + αt * p
+            ft = dE(xt)
+            numdE += 1
+            nft = nkdualnorm(P, ft)
+            iarm += 1
+         end
+         α_old = αt
+      else
+         # if we are here, then p is not a newton direction (i.e. an e-val was
+         # flipped) in this case, we do something very crude:
+         #   take same step as before, then do one line-search step
+         #   and pick the better of the two.
+         αt = 0.66 * α_old  # probably can do better by re-using information from dcg_...
+         αt = min(αt, maxstep / norm(p, Inf))
+         xt = x + αt * p
+         ft = dE(xt)
+         nft = nkdualnorm(P, ft)
+         numdE += 1
+         # find a root: g(t) = (1-t) f0⋅p + t ft ⋅ p = 0 => t = f0⋅p / (f0-ft)⋅p
+         #    if (f0-ft)⋅p is very small, then simply use xt as the next step.
+         #    if it is large enough, then take just one iteration to get a root
+         if abs(dot(f0 - ft, P, p)) > 1e-4   #  TODO: make this a parameter
+            t = dot(f0, P, p) / dot(f0 - ft, P, p)
+            t = max(t, 0.1)    # don't make too small a step
+            t = min(t, 4 * t)  # don't make too large a step
+            αt, αm, nfm, fm = (t*αt), αt, nft, ft
+            αt = min(αt, maxstep / norm(p, Inf))
+            xt = x + αt * p
+            ft = dE(xt)
+            numdE += 1
+            nft = nkdualnorm(P, ft)
+            # if the line-search step is worse than the initial trial, then
+            # we revert
+            if abs(dot(ft, P, p)) > abs(dot(fm, P, p))
+               αt, xt, nft, ft = αm, x + αm * p, nfm, fm
+            end
+         end
+      end
+      if verbose > 3; @show αt; end
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+      # update current configuration and preconditioner
+      x, f0, fnrm = xt, ft, nft
+      P = precon(x)
+      res = norm(f0, Inf)
+      fnrm = nkdualnorm(P, f0)     # should be the dual norm!
+      rat = fnrm/fnrmo
+
+      if verbose > 3; @show λ, res; end
+
+      if res <= tol
+         return x, numdE
+      end
+
+      # ---------------------------------------------------------
+      # Adjust eta as per Eisenstat-Walker.   # TODO: make this a flag!
+      # TODO: check also the we are in the index-1 regime (what do we do if
+      # not? probably reset eta?)
+      etaold = eta
+      etanew = gamma * rat^2
+      if gamma * etaold^2 > 0.1
+         etanew = max(etanew, gamma * etaold^2)
+      end
+      eta = min(etanew, etamax)
+      eta = max(eta, 0.5 * tol / fnrm)
+      # ---------------------------------------------------------
+
+   end
+
+   if verbose > 1
+      warn("NK did not converge within the maximum number of dE evaluations")
+   end
+   return x, numdE
+end
