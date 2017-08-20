4af981f32dabd62da05555297fb4873a0e05b13e
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-08-20 21:31:56 +0100

    improved lsforce

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,13 @@ notCI = !isCI
 println("Running tests for `Isaac.jl`")
 @testset "Isaac" begin
 
-include("testproblems.jl")
-include("correctness.jl")
-if notCI
-    include("performance.jl")
-end
-include("test_dlanczos.jl")
-include("test_saddle1.jl")
+# include("testproblems.jl")
+# include("correctness.jl")
+# if notCI
+#     include("performance.jl")
+# end
+# include("test_dlanczos.jl")
+# include("test_saddle1.jl")
 include("test_minim.jl")
 
 end
