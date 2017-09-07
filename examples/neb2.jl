3f9fd5f9161408ab5345cac05142dca4fcaf5c89
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-09-07 19:48:38 +0100

    examples with succesful preconditioning

--- examples/neb2.jl
+++ examples/neb2.jl
@@ -25,10 +25,10 @@ function vecs2dof(X)
     return x
 end
 
-function fneb(x, k)
+function fneb(x, k, gradE)
     X = dof2vecs(x)
     N = length(X)
-    dE = [Gradient(X[i]) for i=1:N]
+    dE = [gradE(X[i]) for i=1:N]
     # central finite differences
     dxds = [ vecsign(X[i+1]-X[i-1]) for i=2:N-1 ]
     dxds = [ [zeros(d)]; dxds; [zeros(d)] ]
@@ -41,12 +41,14 @@ function fneb(x, k)
     return vecs2dof(dE0⟂ - Fk)
 end
 
-Dfneb(x, k) = ForwardDiff.jacobian(z -> fneb(z,k), x)
+Dfneb(x, k, gradE) = ForwardDiff.jacobian(z -> fneb(z, k, gradE), x)
 
 # setup NEB with spring constant 1 (scaled against number of nodes)
 k = 1.0
-Fneb(x) = fneb(x,k)
-DFneb(x) = Dfneb(x, k)
+Fneb(x) = fneb(x, k, Gradient)
+Fneb0(x) = fneb(x, 0.0, Gradient)
+DFneb(x) = Dfneb(x, k, Gradient)
+Pneb(x, μ) = Dfneb(x, k, x->μ*x)
 
 # load a very good starting guess so we can be sure (hope) to be in the
 # region of attraction of Newton's method
@@ -67,3 +69,53 @@ z_nsoli, nde, ierr, xhist = Isaac.nsoli(x, Fneb, atol=1e-5, rtol=1e-5)
 numF = round(Int, nde[end,2])
 @show norm(Fneb(z_nsoli), Inf)
 @show numF
+
+
+println("Trying NSOLI-STAB")
+x = copy(z)
+μ = 100.0
+P = y -> Pneb(y, μ)
+z_nsoli, numF = Isaac.nsolistab(Fneb, x, tol=1e-5)
+@show norm(Fneb(z_nsoli), Inf)
+@show numF
+
+
+function Pdiag(x)
+    N = length(x) ÷ d
+    P = zeros(length(x), length(x))
+    for n = 1:N
+        In = ((n-1) * 2) .+ (1:2)
+        h = ForwardDiff.jacobian(Gradient, x[In])
+        λ, V = eig(h)
+        P[In, In] = V * diagm(abs.(λ)) * V'
+    end
+    return P
+end
+
+
+println("Trying P-NSOLI-STAB")
+x = copy(z)
+P = y -> Pdiag(y) + Pneb(y, 0.0)
+z_nsoli, numF = Isaac.nsolistab(Fneb, x, tol=1e-5,
+            P = P(x), precon_prep = (P_, x) -> P(x))
+@show norm(Fneb(z_nsoli), Inf)
+@show numF
+
+println("Trying Preconditioned NSOLI")
+x = copy(z)
+pFneb = y ->  P(y) \ Fneb(y)
+z_nsoli, nde, ierr, xhist = Isaac.nsoli(x, pFneb, atol=1e-5, rtol=1e-5)
+numF = round(Int, nde[end,2])
+@show norm(Fneb(z_nsoli), Inf)
+@show numF
+
+
+
+using Plots
+
+J = DFneb(z)
+p = P(z)
+λ, V = eig(p \ J)
+plot(real(λ), imag(λ), lw = 0, m = :o)
+
+minimum(real.(λ))
